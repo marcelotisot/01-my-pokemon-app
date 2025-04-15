0b29f7a65854d9dc905a1990b868d50b97325b3c
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2025-04-14 22:21:48 -0300

    pokemons.controller.spec.ts: se prueba el metodo findAll

--- src/pokemons/pokemons.controller.spec.ts
+++ src/pokemons/pokemons.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonsController } from './pokemons.controller';
 import { PokemonsService } from './pokemons.service';
+import { PaginationDto } from '../shared/dtos/pagination.dto';
 
 describe('PokemonsController', () => {
   let controller: PokemonsController;
+  let service: PokemonsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +14,25 @@ describe('PokemonsController', () => {
     }).compile();
 
     controller = module.get<PokemonsController>(PokemonsController);
+    service = module.get<PokemonsService>(PokemonsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  // Test metodo findAll
+  it('should have called the service with correct parameters', async () => {
+    const dto: PaginationDto = { limit: 10, page: 1 };
+
+    // Espia: crea espia que este pendiente del servicio
+    jest.spyOn(service, 'findAll');
+
+    await controller.findAll(dto);
+
+    // Verifica que findAll haya sido llamado
+    expect(service.findAll).toHaveBeenCalled();
+    // Verificar que haya sido llamado con la informacion del dto
+    expect(service.findAll).toHaveBeenCalledWith(dto);
   });
 });
